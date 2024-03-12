69148ceb52b3593b6f193e2f3946e890c3334155
Nicolás <con23197@uvg.edu.gt>
2024-03-11 19:10:28 -0600

    Correcciones la clase interpreter

--- interpretelisp/src/main/java/uvg/edu/gt/Interpreter.java
+++ interpretelisp/src/main/java/uvg/edu/gt/Interpreter.java
@@ -1,43 +1,22 @@
 package uvg.edu.gt;
-
 import java.util.List;
 
 public class Interpreter {
     private Environment environment;
+    private ExpressionEvaluator evaluator;
 
     public Interpreter() {
         this.environment = new Environment();
+        this.evaluator = new ExpressionEvaluator(environment);
     }
 
-    public Object eval(Object expr) throws Exception {
-        if (expr instanceof List) {
-            List<?> list = (List<?>) expr;
-            if (list.isEmpty()) {
-                throw new Exception("Cannot evaluate an empty list");
-            }
-            String operator = list.get(0).toString();
-            switch (operator) {
-                case "+":
-                    return evalSum(list.subList(1, list.size()));
-                // Agregar casos para "-", "*", "/"
-                default:
-                    throw new Exception("Unknown operator: " + operator);
-            }
-        } else if (expr instanceof Integer) {
-            return expr; // Los números se evalúan a sí mismos
-        } else if (expr instanceof String) {
-            // Agregar lógica para manejar variables y símbolos
-        }
-        throw new Exception("Cannot evaluate expression: " + expr);
+    public void interpret(String expression) throws Exception {
+        Object parsedExpr = Parser.parse(expression);
+        Object result = evaluator.evaluate(parsedExpr);
+        System.out.println("Resultado: " + result);
     }
 
-    private Integer evalSum(List<?> args) throws Exception {
-        int sum = 0;
-        for (Object arg : args) {
-            sum += (Integer) eval(arg); 
-        }
-        return sum;
+    public static void main(String[] args) throws Exception {
+        new Interpreter().interpret("(+ 1 2 3)");
     }
-
-    // Implementar métodos evalSubtract, evalMultiply, evalDivide
 }
