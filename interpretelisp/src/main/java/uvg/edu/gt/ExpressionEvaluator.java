d1da26c683a787599059d4f52b77e683a28f4e6f
Nicolás <con23197@uvg.edu.gt>
2024-03-11 20:16:18 -0600

    Operaciones aritmeticas basicas

--- interpretelisp/src/main/java/uvg/edu/gt/ExpressionEvaluator.java
+++ interpretelisp/src/main/java/uvg/edu/gt/ExpressionEvaluator.java
@@ -25,16 +25,54 @@ public class ExpressionEvaluator {
         if (list.isEmpty()) {
             throw new Exception("no se puede evaluar una lista vacía");
         }
+        if (!(list.get(0) instanceof String)) {
+            throw new Exception("El primer elemento de la lista debe ser un operador en forma de String");
+        }
         String operator = list.get(0).toString();
         List<?> args = list.subList(1, list.size());
 
-        if ("+".equals(operator)) {
-            int sum = 0;
-            for (Object arg : args) {
-                sum += (Integer) evaluate(arg); 
-            }
-            return sum;
+        // Suma
+    if ("+".equals(operator)) {
+        int sum = 0;
+        for (Object arg : args) {
+            sum += (Integer) evaluate(arg);
         }
-        throw new Exception("operador desconocido " + operator);
+        return sum;
+    }
+    // Resta
+    else if ("-".equals(operator)) {
+        if (args.isEmpty()) {
+            throw new Exception("La operación resta requiere al menos un argumento");
+        }
+        int result = (Integer) evaluate(args.get(0));
+        for (int i = 1; i < args.size(); i++) {
+            result -= (Integer) evaluate(args.get(i));
+        }
+        return result;
+    }
+    // Multiplicación
+    else if ("*".equals(operator)) {
+        int product = 1;
+        for (Object arg : args) {
+            product *= (Integer) evaluate(arg);
+        }
+        return product;
+    }
+    // División
+    else if ("/".equals(operator)) {
+        if (args.isEmpty()) {
+            throw new Exception("La operación división requiere al menos un argumento");
+        }
+        int quotient = (Integer) evaluate(args.get(0));
+        for (int i = 1; i < args.size(); i++) {
+            int divisor = (Integer) evaluate(args.get(i));
+            if (divisor == 0) {
+                throw new Exception("División por cero");
+            }
+            quotient /= divisor;
+        }
+        return quotient;
+    }
+    throw new Exception("Operador desconocido: " + operator);
     }
 }
