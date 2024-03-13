f2be8e086f30a1fb512b54de7faa21860652672d
Fernando Hernandez <ferandree270904@gmail.com>
2024-03-12 20:12:24 -0600

    Añadir clase FileUtils para operaciones con archivos
    Introducir la clase utilitaria FileUtils para manejar operaciones comunes con archivos.

---
+++ interpretelisp/src/main/java/uvg/edu/gt/FileUtils.java
@@ -0,0 +1,14 @@
+package uvg.edu.gt;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileUtils {
+
+    public static List<String> readLines(String filePath) throws IOException {
+        return Files.readAllLines(Paths.get(filePath));
+    }
+}
+
