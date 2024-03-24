dcabb6ef00df267515f813166fe7384304530d7d
Nicolás <con23197@uvg.edu.gt>
2024-03-24 17:56:57 -0600

    prubeas unit

--- interpretelisp/src/test/java/uvg/edu/gt/CondTest.java
+++ interpretelisp/src/test/java/uvg/edu/gt/CondTest.java
@@ -3,34 +3,57 @@ package uvg.edu.gt;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+
 class CondTest {
 
     @Test
-    void testGreaterThanOperator() throws Exception {
-        String expression = "(> 3 2)";
-        Object result = Parser.parse(expression);
-        assertTrue((Boolean) result);
-    }
-
-    @Test
-    void testLessThanOperator() throws Exception {
-        String expression = "(< 2 3)";
-        Object result = Parser.parse(expression);
-        assertTrue((Boolean) result);
-    }
-
-    @Test
     void testCondEvaluation() throws Exception {
-        String expression = "(COND ((> 3 2) 'true) ((< 3 2) 'false))";
-        Object result = Parser.parse(expression);
-        assertEquals("true", result);
+        // Preparar el ambiente y el evaluador
+        Environment environment = new Environment();
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(environment);
+
+        // Preparar la expresión COND
+        String condExpression = "(COND ((> 5 3) 'mayor) ((< 2 3) 'menor))";
+
+        // Analizar y evaluar la expresión
+        Object parsedExpression = Parser.parse(condExpression);
+        Object result = evaluator.evaluate(parsedExpression);
+
+        // Verificar el resultado
+        Assertions.assertEquals("mayor", result);
     }
 
     @Test
-    void testCondDefaultCase() throws Exception {
-        String expression = "(COND ((> 2 3) 'false) (T 'true))";
-        Object result = Parser.parse(expression);
-        assertEquals("true", result);
+    void testCondFalseEvaluation() throws Exception {
+        // Ambiente y evaluador
+        Environment environment = new Environment();
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(environment);
+
+        // Una expresión COND donde ninguna condición es verdadera
+        String condExpression = "(COND ((> 2 3) 'mayor) ((< 5 3) 'menor))";
+
+        // Analizar y evaluar
+        Object parsedExpression = Parser.parse(condExpression);
+        Object result = evaluator.evaluate(parsedExpression);
+
+        // Esperamos "NIL" como resultado, ya que ninguna condición se cumple
+        Assertions.assertEquals("NIL", result);
+    }
+
+    @Test
+    void testCondWithVariables() throws Exception {
+        // Este test asume que tu implementación puede manejar variables
+        Environment environment = new Environment();
+        environment.defineVariable("x", 10); // Definir una variable x con valor 10
+        ExpressionEvaluator evaluator = new ExpressionEvaluator(environment);
+
+        String condExpression = "(COND ((> x 5) 'mayor) ((< x 5) 'menor))";
+
+        Object parsedExpression = Parser.parse(condExpression);
+        Object result = evaluator.evaluate(parsedExpression);
+
+        // x es mayor que 5, por lo que esperamos 'mayor' como resultado
+        Assertions.assertEquals("mayor", result);
     }
 }
-
